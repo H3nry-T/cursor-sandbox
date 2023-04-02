e6d08df9cdbf36dfa277db811834b69ba7113a4c
H3nry-T <ht3drex@gmail.com>
2023-04-02 13:34:24 +0100

    remade the index.html file remake.html

--- remake/script.js
+++ remake/script.js
@@ -1,13 +1,26 @@
 (function () {
     var links = document.querySelectorAll("nav > .hover-this");
     var cursor = document.querySelector(".cursor");
-    function animate(e) {
+    function animateCursor(e) {
         cursor.style.left = "".concat(e.clientX, "px");
         cursor.style.top = "".concat(e.clientY, "px");
     }
-    function enlargeCursor() {
-        cursor.style.transform = "scale(8)";
+    function animateSpan(e) {
+        var span = this.querySelector("span");
+        var offsetX = e.offsetX;
+        var offsetY = e.offsetY;
+        var height = this.offsetHeight;
+        var width = this.offsetWidth;
+        var moveX = (offsetX / width) * 50 - 25;
+        var moveY = (offsetY / height) * 50 - 25;
+        span.style.transform = "translate(".concat(moveX, "px,").concat(moveY, "px)");
+        if (e.type === "mouseleave")
+            span.style.transform = "";
     }
-    links.forEach(function (link) { return link.addEventListener("mouseenter", enlargeCursor); });
-    window.addEventListener("mousemove", animate);
+    links.forEach(function (link) { return link.addEventListener("mousemove", animateSpan); } // <-- Change to "mousemove" here
+    );
+    links.forEach(function (link) {
+        return link.addEventListener("mouseleave", animateSpan);
+    });
+    window.addEventListener("mousemove", animateCursor);
 })();
--- remake/script.ts
+++ remake/script.ts
@@ -3,16 +3,33 @@
     document.querySelectorAll("nav > .hover-this");
   const cursor: HTMLDivElement = document.querySelector(".cursor")!;
 
-  function animate(e: MouseEvent) {
+  function animateCursor(e: MouseEvent) {
     cursor.style.left = `${e.clientX}px`;
     cursor.style.top = `${e.clientY}px`;
   }
 
-  function enlargeCursor() {
-    cursor.style.transform = "scale(8)";
+  function animateSpan(this: HTMLElement, e: MouseEvent) {
+    const span: HTMLSpanElement = this.querySelector("span")!;
+
+    const offsetX: number = e.offsetX;
+    const offsetY: number = e.offsetY;
+
+    const height: number = this.offsetHeight;
+    const width: number = this.offsetWidth;
+
+    const moveX: number = (offsetX / width) * 50 - 25;
+    const moveY: number = (offsetY / height) * 50 - 25;
+    span.style.transform = `translate(${moveX}px,${moveY}px)`;
+
+    if (e.type === "mouseleave") span.style.transform = "";
   }
 
-  links.forEach((link) => link.addEventListener("mouseenter", enlargeCursor));
+  links.forEach(
+    (link: HTMLAnchorElement) => link.addEventListener("mousemove", animateSpan) // <-- Change to "mousemove" here
+  );
+  links.forEach((link: HTMLAnchorElement) =>
+    link.addEventListener("mouseleave", animateSpan)
+  );
 
-  window.addEventListener("mousemove", animate);
+  window.addEventListener("mousemove", animateCursor);
 })();
